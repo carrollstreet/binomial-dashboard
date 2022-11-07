b0a68658d31604ef4f4e145c5bf66bf305e1f4ca
Kiryl S <carollgplay@gmail.com>
2022-11-07 20:55:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import gunicorn     
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets, compress=False)
